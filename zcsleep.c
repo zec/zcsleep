333c679fcf4dad5b15b811d637747d0637a17dfb
Zachary Catlin <z@zc.is>
2020-08-06 19:09:34 -0400

    Steps towards a working program

--- zcsleep.c
+++ zcsleep.c
@@ -1,51 +1,64 @@
 #include <stdio.h>
 #include <time.h>
 #include <errno.h>
-#include <string.h>
 
-/* for getpid() and sigaction */
-#include <sys/types.h>
-#include <unistd.h>
-#include <signal.h>
+int timespec_add(struct timespec *accum, const struct timespec *addend);
+int sleep_loop(clockid_t clock_id, const struct timespec *duration);
 
-void null_handler(int x)
-{
-    return;
-}
+int clock_specified = 0;
+clockid_t clockid = CLOCK_REALTIME; /* default to the real-time clock */
 
 int main(int argc, char **argv)
 {
-    struct timespec ts, tr;
-    int ret_val;
-
-    struct sigaction new_act;
-
-    new_act.sa_handler = null_handler;
-    sigfillset(&new_act.sa_mask);
-    new_act.sa_flags = 0;
-    sigaction(SIGUSR1, &new_act, NULL);
-    sigaction(SIGUSR2, &new_act, NULL);
-    sigaction(SIGINT,  &new_act, NULL);
-
-    printf("hello, world\n");
-    printf("PID %llu\n", (unsigned long long) getpid());
+    struct timespec ts;
 
     ts.tv_sec = 5;
     ts.tv_nsec = 0;
 
-    tr.tv_sec = 9999;
-    tr.tv_nsec = 1729;
-
     printf("time to sleep: {%lld,%ld}\n", (long long) ts.tv_sec, ts.tv_nsec);
 
-    errno = 0;
-    ret_val = clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, &tr);
+    return sleep_loop(CLOCK_MONOTONIC, &ts);
+}
 
-    printf("\ntime to sleep: {%lld,%ld}\n", (long long) ts.tv_sec, ts.tv_nsec);
-    printf("remaining time: {%lld,%ld}\n", (long long) tr.tv_sec, tr.tv_nsec);
-    printf("return value: %d (%s)\n", ret_val, strerror(ret_val));
-    printf("errno = %d (%s)\n", errno, strerror(errno));
-    printf("EINTR = %d\n", EINTR);
+#define GIGA ((long) (1000L * 1000L * 1000L))
+
+int timespec_add(struct timespec *accum, const struct timespec *addend)
+{
+    long sec, nsec;
+    if (accum == NULL || addend == NULL)
+        return 0;
+
+    nsec = accum->tv_nsec + addend->tv_nsec;
+    sec  = accum->tv_sec  + addend->tv_sec + (nsec / GIGA);
+
+    if (sec < accum->tv_sec) /* overflow! */
+        return 0;
+
+    accum->tv_sec  = sec;
+    accum->tv_nsec = nsec % GIGA;
+
+    return 1;
+}
+
+/* Given the clock to use and the duration we are supposed to sleep,
+   try sleeping for that duration, handling interruptions due to signals */
+int sleep_loop(clockid_t clock_id, const struct timespec *duration)
+{
+    int err = 0;
+    struct timespec begin, target;
+
+    if(clock_gettime(clock_id, &target) != 0) { /* the start time - now */
+        fputs("Unable to get time for specified clock!\n", stderr);
+        return 1;
+    }
+
+    if (!timespec_add(&target, duration)) /* get our desired wakeup time */
+        return 1;
+
+    while((err = clock_nanosleep(clock_id, TIMER_ABSTIME, &target, NULL)) != 0) {
+        if (err != EINTR)
+            return err;
+    }
 
     return 0;
 }
